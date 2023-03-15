dba9747f9be7775e60f9f0a7f7f591faeec24010
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-03-15 22:51:46 +0300

    changed project

--- src/test/java/FormData.java
+++ src/test/java/FormData.java
@@ -1,25 +1,26 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
-public class FormData {
+class FormData {
     @Test
     public void formData() {
         // Given - When - Then
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("Some") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
-        ;
+                .body("data", equalTo("Some")); //
+
     }
 
 }
 
-}
+
